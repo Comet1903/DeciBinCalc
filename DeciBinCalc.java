b9b1336e7d32524e60711891b391b7f7d6362d4b
Comet1903 <57844706+Comet1903@users.noreply.github.com>
2020-09-04 21:22:20 +0200

    Update DeciBinCalc.java

--- DeciBinCalc.java
+++ DeciBinCalc.java
@@ -7,51 +7,51 @@ public class DeciBinCalc {
         final Scanner scanner = new Scanner(System.in);
         System.out.print("\033[H\033[2J");
         System.out.flush();
-        int dezimalinput;
-        int dezi;
+        int decimalinput;
+        int deci;
         int rest;
-        System.out.println("////////////////////////////////////////////////");
-        System.out.println("// DezBinCalc by Julian-Horn.net (BETA v0.1.2 //");
-        System.out.println("//        Mailto: Info@Julian-Horn.net        //");
-        System.out.println("////////////////////////////////////////////////");
-        System.out.println("//      Note the output is upside down!       //");
-        System.out.println("//    That means if you have 1101 as output   //");
-        System.out.println("//           it means actually 1011           //");
-        System.out.println("////////////////////////////////////////////////");
+        System.out.println("//////////////////////////////////////////////////");
+        System.out.println("// DeciBinCalc by Julian-Horn.net (BETA v0.1.3) //");
+        System.out.println("//         Mailto: Info@Julian-Horn.net         //");
+        System.out.println("//////////////////////////////////////////////////");
+        System.out.println("//      NOTE: the output is upside down!        //");
+        System.out.println("//     That means if you have 1101 as output    //");
+        System.out.println("//            it means actually 1011            //");
+        System.out.println("//////////////////////////////////////////////////");
         System.out.println("  ");
         System.out.println("Type in your number to convert (0 for exit): ");
-        dezimalinput = scanner.nextInt();
-        dezi = dezimalinput;
+        decimalinput = scanner.nextInt();
+        deci = decimalinput;
 
         //calc modolo
     
-        while (dezi > 0){
+        while (deci > 0){
             
             System.out.print("Convertet to binary: ");
-            while (dezi > 0) {
+            while (deci > 0) {
                 
-                rest = dezi % 2;
+                rest = deci % 2;
                 System.out.print(rest);
-                dezi = dezi - rest;
-                dezi = dezi / 2;
+                deci = deci - rest;
+                deci = deci / 2;
             }    
             System.out.println("  ");
             System.out.println("Type a new number to convert (0 for exit): ");
-            dezimalinput = scanner.nextInt();
-            dezi = dezimalinput;
+            decimalinput = scanner.nextInt();
+            deci = decimalinput;
             System.out.print("\033[H\033[2J");
             System.out.flush();
-            System.out.println("////////////////////////////////////////////////");
-            System.out.println("// DezBinCalc by Julian-Horn.net (BETA v0.1.2 //");
-            System.out.println("//        Mailto: Info@Julian-Horn.net        //");
-            System.out.println("////////////////////////////////////////////////");
-            System.out.println("//      Note the output is upside down!       //");
-            System.out.println("//    That means if you have 1101 as output   //");
-            System.out.println("//           it means actually 1011           //");
-            System.out.println("////////////////////////////////////////////////");
+            System.out.println("//////////////////////////////////////////////////");
+            System.out.println("// DeciBinCalc by Julian-Horn.net (BETA v0.1.2) //");
+            System.out.println("//         Mailto: Info@Julian-Horn.net         //");
+            System.out.println("//////////////////////////////////////////////////");
+            System.out.println("//      NOTE: the output is upside down!        //");
+            System.out.println("//     That means if you have 1101 as output    //");
+            System.out.println("//            it means actually 1011            //");
+            System.out.println("//////////////////////////////////////////////////");
             System.out.println("  ");
             System.out.println("Type in your number to convert (0 for exit): ");
-            System.out.println(dezimalinput);
+            System.out.println(decimalinput);
         
 
     }
@@ -63,3 +63,15 @@ public class DeciBinCalc {
     }
     
 }
+
+/** Versions:
+ * BETA v 0.0.1 Calculating one time deci to bin                                 (File: "N/A")
+ * BETA v 0.0.2 Design and comments                                              (File: "N/A")
+ * BETA v 0.0.3 Output the input number                                          (File: "N/A")
+ * BETA v 0.0.4 Dont output overflowed zeros (0)                                 (File: "N/A")
+ * BETA v 0.1.0 Loop Calculation inclufing input                                 (File: "N/A")
+ * BETA v 0.1.1 Loop Calculation including an exit while typing a zero "0"       (File: "N/A")
+ * BETA v 0.1.2 Creating a description                                           (File: "https://github.com/Comet1903/DeziBinCalc")
+*/
+
+
